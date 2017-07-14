b88ee32b3b08d5ecd4724ba189afa7855c2c7fb3
Julio <julio.cesar@kpimkt.com>
2017-07-13 19:12:32 -0500

    Agenda y Web Scraping

---
+++ agenda/contact-inicial.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+def run():
+
+    while True:
+        command = str(raw_input('''
+            ¿Qué deseas hacer?
+
+            [a]ñadir contacto
+            [ac]tualizar contacto
+            [b]uscar contacto
+            [e]liminar contacto
+            [l]istar contactos
+            [s]alir
+        '''))
+
+        if command == 'a':
+            print('añadir contacto')
+
+        elif command == 'ac':
+            print('actualizar contacto')
+
+        elif command == 'b':
+            print('buscar contacto')
+
+        elif command == 'e':
+            print('eliminar contacto')
+
+        elif command == 'l':
+            print('listar contactos')
+
+        elif command == 's':
+            break
+        else:
+            print('Comando no encontrado.')
+
+
+if __name__ == '__main__':
+    run()
